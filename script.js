75f65f227c4d7166aa9bb9fa5f27b9adfdbc36c9
PorThePeople <129730998+PorThePeople@users.noreply.github.com>
2023-04-17 23:57:06 +0700

    Add function to calculate simple math expressions

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
 const display = document.querySelector('#display');
 
-function screenDisplay(input) {
-	display.textContent = `${input}`;
-	console.log(input);
+let string = [];
+let displayString = [];
+
+function numberInput(input) {
+	string.push(input);
+	displayString.push(input);
+	display.textContent = displayString.join('');
+	console.log(string);
+	return string;
+}
+
+function operatorInput(input) {
+	string.push(input);
+	console.log(string);
+	displayString = [];
+	return string;
+}
+
+function operate(input) {
+	regex = /([+-/*]+)/g;
+	operateString = input.join('').split(regex);
+	if (operateString[1] === '+') {
+		result = Number(operateString[0]) + Number(operateString[2]);
+	} else if (operateString[1] === '-') {
+		result = Number(operateString[0]) - Number(operateString[2]);
+	} else if (operateString[1] === '*') {
+		result = Number(operateString[0]) * Number(operateString[2]);
+	} else {
+		result = Number(operateString[0]) / Number(operateString[2]);
+	}
+	display.textContent = result;
+	displayString = [];
+	string = [];
 }
 
 const buttons = document.querySelectorAll('.numbutton');
 buttons.forEach((button) => {
-	button.addEventListener('click', () => screenDisplay(button.id));
+	button.addEventListener('click', () => numberInput(button.id));
 });
+
+const opButtons = document.querySelectorAll('.opbutton');
+opButtons.forEach((button) => {
+	button.addEventListener('click', () => operatorInput(button.id));
+});
+
+const equalButton = document.querySelector('.equalButton');
+equalButton.addEventListener('click', () => operate(string));
